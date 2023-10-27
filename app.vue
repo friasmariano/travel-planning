ca8fabf3a5d127e375d6d311d27b6c98d1294249
Mariano Frías Gómez <mariano.frias@uteco.edu.do>
2023-10-27 13:50:17 +0000

    DevContainer + Bootstrap + FontAwesome

--- app.vue
+++ app.vue
@@ -2,5 +2,13 @@
   <div>
     <!-- Fist commit -->
     Travel Planning App
+
+    <p>This is the way. Let's see how Docker behaves.</p>
+
+    <button class="btn btn-primary">Button</button>
+
+    <div class="alert alert-success" role="alert">
+      <i class="fa-solid fa-circle-info" style="margin-right: 5px;"></i> This way, we don't need to install Boostrap
+    </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,29 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   devServer: {
     port: 8000
+  },
+
+  app: {
+    head: {
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js',
+          integrity: 'sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL',
+          crossorigin: 'anonymous'
+        }
+      ],
+      link: [
+        {
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
+          rel: 'stylesheet',
+          integrity: 'sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN',
+          crossorigin: 'anonymous'
+        },
+        {
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css',
+          rel: 'stylesheet'
+        }
+      ]
+    }
   }
 })
