1eae0a6133e7f8063597624fe461f762d9ee9e6e
Mariano Frías Gómez <mariano.frias@uteco.edu.do>
2023-10-30 15:51:42 +0000

    App updates

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div>
+            <div class="jumbotron text-center">
+                <h1 class="display-4">{{ title }}</h1>
+                <p class="lead">{{ subtitle }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: 'Hello World',
+            subtitle: 'This is a subtitle',
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
