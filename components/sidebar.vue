b8f61b8b2587fea80e387e23a4c91286700c7abb
Mariano Frías Gómez <mariano.frias@uteco.edu.do>
2023-10-30 18:52:31 +0000

    Sidebar addittion

---
+++ components/sidebar.vue
@@ -0,0 +1,89 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">My App</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <router-link to="/" class="nav-link">Home</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/about" class="nav-link">About</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/contact" class="nav-link">Contact</router-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="container-fluid">
+            <div class="row">
+                <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+                    <div class="sidebar-sticky">
+                        <ul class="nav flex-column">
+                            <li class="nav-item">
+                                <router-link to="/" class="nav-link">Home</router-link>
+                            </li>
+                            <li class="nav-item">
+                                <router-link to="/about" class="nav-link">About</router-link>
+                            </li>
+                            <li class="nav-item">
+                                <router-link to="/contact" class="nav-link">Contact</router-link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+                    <router-view></router-view>
+                </main>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Sidebar',
+}
+</script>
+
+<style lang="scss" scoped>
+.sidebar {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 100;
+    padding: 48px 0 0;
+    box-shadow: inset -1px 0 0 rgba(0, 0, 0, .1);
+}
+
+.sidebar-sticky {
+    position: relative;
+    top: 0;
+    height: calc(100vh - 48px);
+    padding-top: .5rem;
+    overflow-x: hidden;
+    overflow-y: auto;
+}
+
+@media (max-width: 767.98px) {
+    .sidebar {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: -100%;
+        z-index: 100;
+        display: block;
+        transition: all 0.3s ease-in-out;
+    }
+
+    .sidebar.show {
+        left: 0;
+        transition: all 0.3s ease-in-out;
+    }
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <header>
+            <!-- Header content goes here -->
+        </header>
+        <aside>
+            <sidebar />
+        </aside>
+        <main>
+            <!-- Main content goes here -->
+        </main>
+        <footer>
+            <!-- Footer content goes here -->
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Default'
+}
+</script>
+
+<style>
+/* Global styles go here */
+</style>
