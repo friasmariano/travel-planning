91cf4bd9ca1d2b4b175789e46c0cbbe932703286
Mariano Frias <frias.mariano@gmail.com>
2024-02-12 07:10:08 -0400

    Layout definition and Navbar update

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <header>
+            
+        </header>
+
+        <aside>
+
+        </aside>
+
+        <main>
+
+        </main>
+            <slot />
+        <footer>
+
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
