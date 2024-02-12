8de3daf2c899122f34903f254859fd03ecc5c52a
Mariano Frías Gómez <mariano.frias@uteco.edu.do>
2024-02-12 17:27:06 -0400

    Sidebar+ design updates

---
+++ components/Sidebar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="sidebar">
+        <button class="button">Travel Highlights</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.button {
+    width: 150px;
+    background-color: grey;
+    border-radius: 20px;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.sidebar {
+    height: 100vh;
+    width: 220px;
+
+    padding-left: 30px;
+    padding-top: 30px;
+
+    background-color: rgb(242, 242, 242);
+
+    box-shadow: 0.5px 0px 20px rgba(0,0,0,0.2);
+}
+</style>
--- pages/playground.vue
+++ pages/playground.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
         <Navbar />
+
+        <Sidebar />
     </div>
 </template>
 
