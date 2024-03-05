ca613096ee3a72afaa36934e45ad311d22ca25c1
Mariano Frías Gómez <mariano.frias@uteco.edu.do>
2024-03-05 17:44:34 -0400

    Responsive icons set - sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,132 +1,262 @@
 <template>
-    <div class="sidebar">
-        <button class="button" @mouseover="filledHomeIcon" @mouseleave="regularHomeIcon">
-            <i :class="homeIcon" style="margin-right: 5px;"></i> 
-            Home
-        </button>
-        <button class="button" @mouseover="filledProfileIcon" @mouseleave="regularProfileIcon">
-            <i :class="profileIcon" style="margin-right: 5px;"></i> 
-            Profile
-        </button>
-        <button class="button" @mouseover="filledDestinationsIcon" @mouseleave="regularDestinationsIcon">
-            <i :class="destinationsIcon" style="margin-right: 5px;"></i> 
-            Destinations
-        </button>
-        <button class="button" @mouseover="filledItineraryIcon" @mouseleave="regularItineraryIcon">
-            <i :class="itineraryIcon" style="margin-right: 5px;"></i> 
-            Itinerary
-        </button>
-        <button class="button" @mouseover="filledWeatherIcon" @mouseleave="regularWeatherIcon">
-            <i :class="weatherIcon" style="margin-right: 5px;"></i> 
-            Weather
-        </button>
-        <button class="button" @mouseover="filledRecommendationsIcon" @mouseleave="regularRecommendationsIcon">
-            <i :class="recommendationsIcon" style="margin-right: 5px;"></i> 
-            Recommendations
-        </button>
-</div>
+  <div class="sidebar">
+    <button
+      class="button"
+      @mouseover="filledHomeIcon"
+      @mouseleave="regularHomeIcon"
+    >
+      <i :class="homeIcon" style="margin-right: 5px"></i>
+      <span class="button-text">Home</span>
+    </button>
+    <button
+      class="button"
+      @mouseover="filledProfileIcon"
+      @mouseleave="regularProfileIcon"
+    >
+      <i :class="profileIcon" style="margin-right: 5px"></i>
+      <span class="button-text">Profile</span>
+    </button>
+    <button
+      class="button"
+      @mouseover="filledDestinationsIcon"
+      @mouseleave="regularDestinationsIcon"
+    >
+      <i :class="destinationsIcon" style="margin-right: 5px"></i>
+      <span class="button-text">Destinations</span>
+    </button>
+    <button
+      class="button"
+      @mouseover="filledItineraryIcon"
+      @mouseleave="regularItineraryIcon"
+    >
+      <i :class="itineraryIcon" style="margin-right: 5px"></i>
+      <span class="button-text">Itinerary</span>
+    </button>
+    <button
+      class="button"
+      @mouseover="filledWeatherIcon"
+      @mouseleave="regularWeatherIcon"
+    >
+      <i :class="weatherIcon" style="margin-right: 5px"></i>
+      <span class="button-text">Weather</span>
+    </button>
+    <button
+      class="button"
+      @mouseover="filledRecommendationsIcon"
+      @mouseleave="regularRecommendationsIcon"
+    >
+      <i :class="recommendationsIcon" style="margin-right: 5px"></i>
+      <span class="button-text">Recommendations</span>
+    </button>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                homeIcon: 'bi bi-house',
-                profileIcon: 'bi bi-person',
-                destinationsIcon: 'bi bi-pin-map',
-                itineraryIcon: 'bi bi-calendar4-event',
-                weatherIcon: 'bi bi-cloud-sun',
-                recommendationsIcon: 'bi bi-lightbulb'
-            }
-        },
-        methods: {
-            // Home icon
-            filledHomeIcon() {
-                this.homeIcon = 'bi bi-house-fill'
-            },
-            regularHomeIcon() {
-                this.homeIcon = 'bi bi-house'
-            },
-            // Profile icon
-            filledProfileIcon() {
-                this.profileIcon = 'bi bi-person-fill'
-            },
-            regularProfileIcon() {
-                this.profileIcon = 'bi bi-person'
-            },
-            // Destinations icon
-            filledDestinationsIcon() {
-                this.destinationsIcon = 'bi bi-pin-map-fill'
-            },
-            regularDestinationsIcon() {
-                this.destinationsIcon = 'bi bi-pin-map'
-            },
-            // Itinerary icon
-            filledItineraryIcon() {
-                this.itineraryIcon = 'bi bi-calendar-event-fill'
-            },
-            regularItineraryIcon() {
-                this.itineraryIcon = 'bi bi-calendar4-event'
-            },
-            // Weather icon
-            filledWeatherIcon() {
-                this.weatherIcon = 'bi bi-cloud-sun-fill'
-            },
-            regularWeatherIcon() {
-                this.weatherIcon = 'bi bi-cloud-sun'
-            },
-            // Recommendations icon
-            filledRecommendationsIcon() {
-                this.recommendationsIcon = 'bi bi-lightbulb-fill'
-            },
-            regularRecommendationsIcon() {
-                this.recommendationsIcon = 'bi bi-lightbulb'
+import { onMounted, onBeforeUnmount, ref, watch } from 'vue'
+
+export default {
+    setup() {
+        let homeIcon = ref('bi bi-house');
+        let profileIcon = ref('bi bi-person');
+        let destinationsIcon = ref('bi bi-pin-map');
+        let itineraryIcon = ref('bi bi-calendar4-event');
+        let weatherIcon = ref('bi bi-cloud-sun');
+        let recommendationsIcon = ref('bi bi-lightbulb');
+
+        let currentWidth = ref(0);
+        let currentHeight = ref(0);
+
+        const trackSize = () => {
+            currentWidth.value = window.innerWidth;
+            currentHeight.value = window.innerHeight;
+        }
+
+        const onResize = (event) => {
+            trackSize();
+        }
+
+        onMounted(() => {
+            window.addEventListener('resize', onResize);
+            // setIcons();
+            
+            trackSize();
+
+            setIcons();
+
+            console.log('Width: ' + currentWidth.value);
+
+
+        })
+
+        onBeforeUnmount(() => {
+            window.removeEventListener('resize', onResize);
+        })
+
+        let isSmall = ref(false)
+
+        // Home icon
+        const filledHomeIcon = () => {
+            homeIcon.value = "bi bi-house-fill";
+        }
+        const regularHomeIcon = () => {
+            homeIcon.value = "bi bi-house";
+        }
+
+        // Profile icon
+        const filledProfileIcon = () => {
+            profileIcon.value = "bi bi-person-fill";
+        }
+        const regularProfileIcon = () => {
+            profileIcon.value = "bi bi-person";
+        }
+
+        // Destinations icon
+        const filledDestinationsIcon = () => {
+            destinationsIcon.value = "bi bi-pin-map-fill";
+        }
+        const regularDestinationsIcon = () => {
+            destinationsIcon.value = "bi bi-pin-map";
+        }
+
+        // Itinerary icon
+        const filledItineraryIcon = () => {
+            itineraryIcon.value = "bi bi-calendar-event-fill";
+        }
+        const regularItineraryIcon = () => {
+            itineraryIcon.value = "bi bi-calendar4-event";
+        }
+
+        // Weather icon
+        const filledWeatherIcon = () => {
+            weatherIcon.value = "bi bi-cloud-sun-fill";
+        }
+        const regularWeatherIcon = () => {
+            weatherIcon.value = "bi bi-cloud-sun";
+        }
+
+        // Recommendations icon
+        const filledRecommendationsIcon = () => {
+            recommendationsIcon.value = "bi bi-lightbulb-fill";
+        }
+        const regularRecommendationsIcon = () => {
+            recommendationsIcon.value = "bi bi-lightbulb";
+        }
+
+        const setIcons = () => {
+            if (currentWidth.value <= 600) {
+                filledHomeIcon()
+                filledProfileIcon()
+                filledDestinationsIcon()
+                filledItineraryIcon()
+                filledWeatherIcon()
+                filledRecommendationsIcon()
+            } else {
+                regularHomeIcon()
+                regularProfileIcon()
+                regularDestinationsIcon()
+                regularItineraryIcon()
+                regularWeatherIcon()
+                regularRecommendationsIcon()
             }
         }
+
+        watch(currentWidth, (newValue, oldValue) => {
+            setIcons()
+        })
+
+        return {
+            homeIcon,
+            profileIcon,
+            destinationsIcon,
+            itineraryIcon,
+            weatherIcon,
+            recommendationsIcon,
+            onMounted,
+            onBeforeUnmount,
+            isSmall,
+            trackSize,
+            onResize,
+            filledHomeIcon,
+            regularHomeIcon,
+            filledProfileIcon,
+            regularProfileIcon,
+            filledDestinationsIcon,
+            regularDestinationsIcon,
+            filledItineraryIcon,
+            regularItineraryIcon,
+            filledWeatherIcon,
+            regularWeatherIcon,
+            filledRecommendationsIcon,
+            regularRecommendationsIcon
+        }
     }
+}
 </script>
 
 <style lang="scss" scoped>
 .button {
-    width: 290px;
-    height: 50px;
+  width: 280px;
+  height: 50px;
 
-    text-align: left;
+  text-align: left;
 
-    padding-left: 40px;
+  padding-left: 40px;
 
-    margin-bottom: 32px;
+  margin-bottom: 32px;
 
-    // color: rgb(10, 73, 91);
-    color: rgba(15, 95, 117, 0.9);
+  // color: rgb(10, 73, 91);
+  color: rgba(18, 115, 142, 0.9);
 
-    background-color: rgb(244, 244, 244);
-    border-radius: 0px 32px 32px 0px;
+  background-color: rgb(244, 244, 244);
+  border-radius: 0px 32px 32px 0px;
 }
 
 .button:hover {
-    color: white;
-    // background-color: rgb(189, 189, 189);
-    background: linear-gradient(to left, rgb(10, 73, 91) , rgb(19, 123, 151), rgb(28, 165, 203));
+  color: white;
+  // background-color: rgb(189, 189, 189);
+  background: linear-gradient(
+    to left,
+    rgb(10, 73, 91),
+    rgb(19, 123, 151),
+    rgb(28, 165, 203)
+  );
 
-    font-weight: 600;
+  font-weight: 600;
 
-    box-shadow: 0px 1px 8px 0px rgba(53, 53, 53, 0.4);
+  box-shadow: 0px 1px 8px 0px rgba(53, 53, 53, 0.4);
 }
 
 .icon {
-    margin-right: 5px;
+  margin-right: 5px;
 }
 
 .sidebar {
-    height: 90vh;
-    width: 300px;
+  height: 90vh;
+  width: 290px;
 
-    padding-left: 0px;
-    padding-top: 50px;
+  padding-left: 0px;
+  padding-top: 50px;
 
-    background-color: rgb(244, 244, 244);
+  background-color: rgb(244, 244, 244);
 
-    box-shadow: 0.5px 0px 20px rgba(0,0,0,0.2);
+  box-shadow: 0.5px 0px 20px rgba(0, 0, 0, 0.2);
+}
+
+.button-text {
+  display: inline-block;
+}
+
+@media (max-width: 600px) {
+  .sidebar {
+    width: 100px;
+  }
+
+  .button {
+    width: 92px;
+  }
+
+  .button-text {
+    display: none;
+  }
 }
 </style>
