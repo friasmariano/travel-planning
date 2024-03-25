95ab0d5da3b7eb9c2b894ebcb166dc7bef011967
Mariano Frías Gómez <mariano.frias@uteco.edu.do>
2024-03-25 15:23:40 -0400

    Navbar testing - ready

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -24,10 +24,9 @@
                                 <!-- Profile dropdown -->
                                 <div class="relative ml-3">
                                     <div>
-                                        <button @click="burger.changeStatus()" type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                        <button id="profileIcon" @click="burgerStore.changeStatus()" type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" aria-expanded="false" aria-haspopup="true">
                                             <span class="absolute -inset-1.5"></span>
-                                            <span class="sr-only">Open user menu</span>
-                                            <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                                            <img id="profileImage" class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
                                         </button>
                                     </div>
                                 </div>
@@ -46,14 +45,14 @@
 </template>
 
 <script>
-// import { useBurgerMenuStore } from '@/stores/burgerMenu'
+import { useBurgerMenuStore } from '@/stores/burgerMenu'
 
     export default {
         setup() {
-            // const burgerStore = useBurgerMenuStore()
+            const burgerStore = useBurgerMenuStore()
 
             return {
-                // burgerStore
+                burgerStore
             }
         }
     }
--- components/tests/Navbar.spec.ts
+++ components/tests/Navbar.spec.ts
@@ -1,12 +1,18 @@
 
 import { describe, expect, test } from 'vitest'
+import { createPinia } from 'pinia'
 import { mount } from '@vue/test-utils'
 import Navbar from '../Navbar.vue'
+import fetch from 'node-fetch'
 
-const wrapper = mount(Navbar);
-const brandLogo = wrapper.find('#brandLogo')
+const pinia = createPinia()
+
+const wrapper = mount(Navbar, { global: { plugins: [pinia] }})
+
 
 describe('Navbar', () => {
+    const brandLogo = wrapper.find('#brandLogo')
+
     test('It has a logo container', () => {
         expect(brandLogo.exists()).toBe(true)
     })
@@ -15,5 +21,24 @@ describe('Navbar', () => {
         const logoImageSrc = brandLogo.attributes('src')
 
         expect(logoImageSrc).toBe('/img/logo.png')
+    })
+
+    test('It has a profile image', () => {
+        const profileIcon = wrapper.find('#profileIcon')
+        expect(profileIcon.exists()).toBe(true)
+
+        const profileImage = wrapper.find('#profileImage')
+        expect(profileImage.exists()).toBe(true)
+
+        console.log('Image was loaded successfully')
+    })
+
+    test('Profile image is valid', async () => {
+        const userImage = wrapper.find('#profileImage')
+        const imageURL = userImage.attributes('src')
+
+        const response = await fetch(imageURL + '')
+        expect(response.status).toBe(200)
+
     })
 })
