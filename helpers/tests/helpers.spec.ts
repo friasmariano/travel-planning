5b9fd05cf8c1f5909be532e146ce634c1ab0c013
Mariano Frías Gómez <mariano.frias@uteco.edu.do>
2024-04-11 17:39:38 -0400

    Firebase set up update

--- helpers/tests/helpers.spec.ts
+++
@@ -1,9 +0,0 @@
-
-import { describe, expect, test } from 'vitest'
-import helpers from '../helpers'
-
-describe('Helpers', () => {
-    test("Contains a 'helloWorld' function", async () => {
-        expect(typeof helpers.helloWorld).toBe('function')
-    })
-})
--- plugins/firebase.client.ts
+++ plugins/firebase.client.ts
@@ -1,9 +1,20 @@
 import { initializeApp } from "firebase/app"
 
-export default defineNuxtPlugin(nuxtApp => {
-    const firebaseConfig = {
+const firebaseConfig = {
+    apiKey: "AIzaSyBsOac8WumS9LZmYSKRoaXYJ_Scobb1OC4",
 
-    }
-    
-    const app = initializeApp(firebaseConfig)
-})
+    authDomain: "travel-planning-37b0d.firebaseapp.com",
+  
+    projectId: "travel-planning-37b0d",
+  
+    storageBucket: "travel-planning-37b0d.appspot.com",
+  
+    messagingSenderId: "562416699739",
+  
+    appId: "1:562416699739:web:b07b28f78283ba3eadf27e",
+  
+    measurementId: "G-33NPH1V13L"
+  
+}
+
+const app = initializeApp(firebaseConfig)
